7e5068d7fd34064432602388e6e2731eea726bd2
shin-migami <davsuvo@gmail.com>
2016-09-14 20:33:59 +0530

    :books:Add Comments Cycle Finder Algo

--- CurveSim/CycleFinder.cpp
+++ CurveSim/CycleFinder.cpp
@@ -8,18 +8,25 @@ CycleFinder::CycleFinder(Graph& G)
 {
 	onStack.assign(G.v_count(), false);
 	ignore.assign(G.v_count(),  false);
+	//store all cycles found in an iteration
 	std::vector<SimpleCycle> fromV;
+	//Start from all vertices and find cycles 
+	//which starts from and end on it
 	for (auto vMap : G.vertex())
 	{
 		SimpleCycle c;
 		dfs(G, vMap, fromV, c);
+		//If traversal has found cycles
 		if (!fromV.empty())
 		{
+			//Find the Cycle with least average latency
+			//That will be our greedy cycle
 			std::make_heap(fromV.begin(), fromV.end(), SimpleCycle());
 			greedy.push_back(fromV.front());
 			simple.insert(simple.end(), fromV.begin(), fromV.end());
 			fromV.clear();
 		}
+		//Put the explored node on ignore list
 		ignore[vMap.second] = true;
 	}
 	//Lowest latency Cycle is moved to the top
@@ -33,16 +40,23 @@ void CycleFinder::dfs(Graph& G,
 					  SimpleCycle& c)
 {
 	size_t v = vMap.second;
+	//Push vertex on a stack and mark it
 	stack.push_back(v);
 	onStack[v] = true;
+	//Iterate over all adjacent vertices
 	for (auto edge : G.adj(vMap.first))
 	{
 		size_t w = edge.to();
+		//If its marked indicated its on the current path
+		//And thus should not be explored
+		//We have found all cycles for nodes on ignore list
+		//so no need to traverse them
 		if (!onStack[w] && !ignore[w])
 		{
 			c.add_edge(edge);
 			std::pair<std::bitset<STATES>, size_t> e(edge.to_state(),
 													 edge.to());
+			//Traverse adjacent node recusively
 			dfs(G, e, fromV, c);
 		}
 		else if (stack[0] == w && !ignore[w])
@@ -54,6 +68,8 @@ void CycleFinder::dfs(Graph& G,
 			c = SimpleCycle();
 		}
 	}
+	//Push it of the stack for further exploration
+	//And unmark it
 	stack.pop_back();
 	onStack[v] = false;
 }
